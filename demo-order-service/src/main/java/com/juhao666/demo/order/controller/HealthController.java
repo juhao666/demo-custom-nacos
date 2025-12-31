89034749b3ef61bd1ddcf8ec6b7efdfc4c91ed21
Erik Liu <liu_jianying1985@hotmail.com>
2025-12-31 16:39:35 +0800

    use config listener in other 2 services success

--- demo-order-service/src/main/java/com/juhao666/demo/order/controller/HealthController.java
+++ demo-order-service/src/main/java/com/juhao666/demo/order/controller/HealthController.java
@@ -3,6 +3,7 @@ package com.juhao666.demo.order.controller;
 import com.juhao666.asac.model.Response;
 import com.juhao666.asac.model.Result;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +13,21 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@RefreshScope
 public class HealthController {
 
     @Value("${registry.instance.service-name}")
     private String serviceName;
     @Value("${registry.instance.instance-id}")
     private String instanceId;
+    @Value("${order.name}")
+    private String orderName;
 
     @GetMapping("/health")
     public Result health() {
         Map<String, Object> data = new HashMap<>();
         data.put("status", "UP");
+        data.put("orderName",orderName);
         data.put("service", serviceName);
         data.put("instanceId", instanceId);
         data.put("timestamp", System.currentTimeMillis());
--- demo-product-service/src/main/java/com/juhao666/demo/product/controller/HealthController.java
+++ demo-product-service/src/main/java/com/juhao666/demo/product/controller/HealthController.java
@@ -3,6 +3,7 @@ package com.juhao666.demo.product.controller;
 import com.juhao666.asac.model.Response;
 import com.juhao666.asac.model.Result;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +13,20 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@RefreshScope
 public class HealthController {
 
     @Value("${registry.instance.service-name}")
     private String serviceName;
     @Value("${registry.instance.instance-id}")
     private String instanceId;
-
+    @Value("${product.name}")
+    private String productName;
     @GetMapping("/health")
     public Result health() {
         Map<String, Object> data = new HashMap<>();
         data.put("status", "UP");
+        data.put("productName",productName);
         data.put("service", serviceName);
         data.put("instanceId", instanceId);
         data.put("timestamp", System.currentTimeMillis());
